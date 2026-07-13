ee6453b935d2a30d3ee178a3c73993c1fd486525
Vitaly Ponomaryov <vitas.ponomaryov1604232@gmail.com>
2026-07-13 15:55:29 +0300

    P0 исправления: <version>3.5.16</version> на <version>3.3.13</version>

--- dev-up.ps1
+++ dev-up.ps1
@@ -1,4 +1,12 @@
 Write-Host "Starting ECommerce Platform..." -ForegroundColor Green
-docker-compose up -d
+
+docker compose up -d
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Startup failed!" -ForegroundColor Red
+    exit 1
+}
+
 Write-Host "All services started!" -ForegroundColor Green
-docker-compose ps
+
+docker compose ps
